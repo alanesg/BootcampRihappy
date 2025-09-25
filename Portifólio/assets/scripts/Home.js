9f0b82a7c10789bbc12deef7ee126639da975fe9
alanesg <alanesGsilva@gmail.com>
2025-09-25 12:55:39 -0300

    codando

---
+++ Portifólio/assets/scripts/Home.js
@@ -0,0 +1,6 @@
+/*função para abrir a aba de projetos*/
+document.querySelector('.dropdown-toggle').addEventListener('click', function(event) {
+  event.preventDefault();
+  var dropdownMenu = document.querySelector('.dropdown-menu');
+  dropdownMenu.classList.toggle('show');
+});
