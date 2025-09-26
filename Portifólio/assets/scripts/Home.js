d1bc99d9e82d250aad2104c8f37fe384b3e6db05
alanesg <alanesGsilva@gmail.com>
2025-09-25 22:03:44 -0300

    portfolio

--- Portifólio/assets/scripts/Home.js
+++ Portifólio/assets/scripts/Home.js
@@ -2,5 +2,8 @@
 document.querySelector('.dropdown-toggle').addEventListener('click', function(event) {
   event.preventDefault();
   var dropdownMenu = document.querySelector('.dropdown-menu');
+  
   dropdownMenu.classList.toggle('show');
+
+  this.classList.toggle('active');
 });
