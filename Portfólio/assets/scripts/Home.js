0d9ecdfb658e8e1f5d477715f1ee806dd30050c0
alanesg <alanesGsilva@gmail.com>
2025-10-02 16:57:43 -0300

    Portfólio

---
+++ Portfólio/assets/scripts/Home.js
@@ -0,0 +1,18 @@
+/*função para abrir a aba de projetos*/
+document.querySelector('.dropdown-toggle').addEventListener('click', function(event) {
+  event.preventDefault();
+  var dropdownMenu = document.querySelector('.dropdown-menu');
+  
+  dropdownMenu.classList.toggle('show');
+
+  this.classList.toggle('active');
+
+  
+});
+
+/*função com condicionnal IF para deixar o menu em negrito */
+
+
+
+
+
