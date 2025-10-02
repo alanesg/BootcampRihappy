512ef67fd79c0de421b2bdf10370473e2164785d
alanesg <alanesGsilva@gmail.com>
2025-10-02 19:04:43 -0300

    Projeto Concluído

--- Portfólio/assets/scripts/Home.js
+++ Portfólio/assets/scripts/Home.js
@@ -1,16 +1,18 @@
 /*função para abrir a aba de projetos*/
 document.querySelector('.dropdown-toggle').addEventListener('click', function(event) {
   event.preventDefault();
-  var dropdownMenu = document.querySelector('.dropdown-menu');
-  
+
+  var dropdownMenu = document.querySelector('.dropdown-menu'); //pega o dropdown menu
+  var caret = this.querySelector('.caret'); //pega a setinha do dropdown
+
   dropdownMenu.classList.toggle('show');
 
-  this.classList.toggle('active');
+  this.classList.toggle('active'); //aba de dropdown ativada
 
-  
+  caret.classList.toggle('rotate'); //rotação da setinha
 });
 
-/*função com condicionnal IF para deixar o menu em negrito */
+
 
 
 
